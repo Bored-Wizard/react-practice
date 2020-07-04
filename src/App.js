4209f982c87805ff7a6d92c0aa1154700403a2e5
sujan-arm64 <sujanbarman12345q@gmail.com>
2020-07-04 18:26:29 +0000

    Third day, learnt about states and prop

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import NameComponent from './components/NameComponent'
 
 class App extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            name : "sujan",
+            job : "programmer",
+        };
+    }
+
+    click(){
+        this.setState({
+            name : "Sujan Barman",
+            job : "backender",
+        })
+    }
+
     componentDidMount(){
         console.log("mounted")
     }
     render() {
         return (
         <div className="App">
-            <p>Hello</p>
+            <p>{this.state.name} - {this.state.job}</p>
             <div>
                 <NameComponent/>
-                <button onClick={ () => {console.log("Hey")}}>Hey</button>
+                <button onClick={ 
+                    this.click.bind(this)
+                }>ok</button>
             </div>
         </div>
         );
